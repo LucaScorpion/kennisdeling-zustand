cb79b9286b39f335a9d1fe4bbc60f53def13a070
Luca Scalzotto <luca@scorpiac.com>
2022-06-09 21:35:25 +0200

    Why not redux

--- src/slides/WhyNotRedux.tsx
+++ src/slides/WhyNotRedux.tsx
@@ -5,8 +5,8 @@ export const WhyNotRedux: React.FC = () => (
     <h2>Waarom niet Redux?</h2>
     <ul>
       <li>Redux heeft veel boilerplate</li>
-      <li>Async operaties zijn niet <em>nog</em> meer boilerplate</li>
-      <li>Makkelijker te begrijpen voor minder ervaren frontenders</li>
+      <li>Async operaties (thunks) zijn <em>nog</em> meer boilerplate</li>
+      <li>Zustand is (imo) makkelijker te begrijpen voor minder ervaren frontenders</li>
     </ul>
     <p>
       Note: je <em>kan</em> Zustand wel zo inrichten dat je het zoals Redux kan gebruiken (i.e. reducers, types, etc).
